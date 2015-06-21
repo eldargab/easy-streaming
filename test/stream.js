dab38b63dc391d2f729cfd672db1e264679078d1
Eldar Gabdullin <eldargab@gmail.com>
2015-06-21 13:04:59 +0300

    tests & fixes

---
+++ test/stream.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var join = require('path').join
+var should = require('should')
+var Stream = require('../stream')
+
+describe('Easy streaming', function() {
+  it('Integration test', function(done) {
+    var file = fs.createReadStream(join(__dirname, 'stream.js'))
+    var stream = Stream.sanitize(file)
+    Stream.buffer(stream, {encoding: 'utf8'}).next(function(err, str) {
+      if (err) return done(err)
+      str.should.match(/Integration test/)
+      done()
+    })
+  })
+})
